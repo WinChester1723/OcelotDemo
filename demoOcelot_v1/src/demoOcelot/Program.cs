88cb8b0d0c32c8b1151b80504cfc49fd25718253
Winchester1723 <pasha.pashazade@novum.tech>
2024-07-19 18:10:46 +0400

    Init Commit

---
+++ demoOcelot_v1/src/demoOcelot/Program.cs
@@ -0,0 +1,22 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add configuration from ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Add Ocelot services
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Use Ocelot middleware
+await app.UseOcelot();
+
+app.Run();
